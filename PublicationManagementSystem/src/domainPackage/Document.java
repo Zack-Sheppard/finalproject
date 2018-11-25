a44018a457e0029195eac9c214fc26d0a3173562
shayan7972 <zamani.shayan@yahoo.com>
2018-11-25 14:53:23 -0700

    RQ2 - operator can add, remove and update

--- PublicationManagementSystem/src/domainPackage/Document.java
+++ PublicationManagementSystem/src/domainPackage/Document.java
@@ -5,18 +5,62 @@ import java.util.Date;
 
 public class Document {
     private int id;
+    private String title;
     private boolean onSale;
     private double price;
     private ArrayList<Author> authors;
-    private Date publishDate;
     private String type;
     private String genre;
     private String publisher;
-    private int numOfPages;
 
+    public Document(int id, String title, String type, String genre, double price, String publisher){
+        this.id=id;
+        this.title=title;
+        this.type=type;
+        this.genre=genre;
+        this.price=price;
+        this.publisher=publisher;
+
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+
+
+    public ArrayList<Author> getAuthors() {
+        return authors;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 
 
     public boolean isOnSale() {
         return onSale;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String toString(){
+        String doc =this.id + " " + this.title + " " + this.type + " " + this.price;
+        return doc;
     }
 }
